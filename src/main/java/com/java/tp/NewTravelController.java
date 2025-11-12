e1927cc0b4accdad3ecff8e3f62d3a612829c05b
gasparpuentevillarroel <gasparpuentevillarroel2006@gmail.com>
2025-11-12 18:06:20 -0300

    inicio travel

---
+++ src/main/java/com/java/tp/NewTravelController.java
@@ -0,0 +1,12 @@
+package com.java.tp;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class NewTravelController {
+
+    @FXML
+    private void switchTosegundEtapaCrearViajoLargaDis() throws IOException {
+        App.setRoot("segundEtapaCrearViajoLargaDis");
+    }
+}

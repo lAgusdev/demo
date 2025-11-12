44f1eb8858589db55c0b3c86f5103da0f1a3532e
Agustin Moar <134026222+lAgusdev@users.noreply.github.com>
2025-11-12 18:37:38 -0300

    Funciona la listita

--- src/main/java/com/java/tp/NewTravelController.java
+++ src/main/java/com/java/tp/NewTravelController.java
@@ -2,8 +2,17 @@ package com.java.tp;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 public class NewTravelController {
+
+    @FXML
+    private ComboBox<String> opcionesComboBox;
+
+    @FXML
+    private void initialize() {
+        opcionesComboBox.getItems().setAll("Opción 1", "Opción 2", "Opción 3");
+    }
 
     @FXML
     private void switchToSecondInstanceCreateTravel() throws IOException {

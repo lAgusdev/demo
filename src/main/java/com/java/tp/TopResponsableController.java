c2ffffbdcdc38560e6660a5edb4e575702e7faf4
Agustin Moar <134026222+lAgusdev@users.noreply.github.com>
2025-11-12 20:40:05 -0300

    Se agregaron las listas para cada uno de los reportes

--- src/main/java/com/java/tp/TopResponsableController.java
+++ src/main/java/com/java/tp/TopResponsableController.java
@@ -1,10 +1,23 @@
 package com.java.tp;
 
 import java.io.IOException;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 public class TopResponsableController {
+
+    @FXML
+    private javafx.scene.control.ListView<String> list;
+
+    @FXML
+    private void initialize() {
+        List<String> lista = List.of("Alejandro", "Alfonso", "Maria", "Gonzalez", "Pedro", "Luis", "Martinez", "Ana", "Lucia", "Rodriguez", "Carlos", "Javier", "Lopez", "Sofia", "Elena");
+        ObservableList<String> items = FXCollections.observableArrayList(lista);
+        list.setItems(items);
+    }
 
     @FXML
     private void switchToMainMenu() throws IOException {
--- src/main/java/com/java/tp/TopVehiclesController.java
+++ src/main/java/com/java/tp/TopVehiclesController.java
@@ -1,10 +1,23 @@
 package com.java.tp;
 
 import java.io.IOException;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
 public class TopVehiclesController {
+
+    @FXML
+    private javafx.scene.control.ListView<String> list;
+
+    @FXML
+    private void initialize() {
+        List<String> lista = List.of("Auto", "Camión", "Motocicleta", "Bicicleta", "Autobús");
+        ObservableList<String> items = FXCollections.observableArrayList(lista);
+        list.setItems(items);
+    }
 
     @FXML
     private void switchToMainMenu() throws IOException {

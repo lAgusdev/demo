87241617a9fd5546a29f6373e547b53cb9dedb3f
Agustin Moar <134026222+lAgusdev@users.noreply.github.com>
2025-11-13 18:02:44 -0300

    Se "instalo" jakarta

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.java.tp {
     requires transitive javafx.graphics;
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
+    requires transitive jakarta.xml.bind;
     exports com.java.tp.guiControllers; 
     opens com.java.tp.guiControllers to javafx.fxml;
     exports com.java.tp;
